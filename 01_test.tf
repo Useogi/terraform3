e8b1c13b0c4e8ab0495ba779a139b95b365d29f7
Useogi <unp2093@naver.com>
2021-11-25 11:40:37 +0900

    'step3'

---
+++ 01_test.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "ap-northeast-2"
+}
+
+resource "aws_vpc" "useogi_vpc" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+
+    tags = {
+        "Name" = "useogi-vpc"
+    }
+}
