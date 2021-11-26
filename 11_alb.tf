6c684761e2fcc034e875e3905c60fc4b186fba91
Useogi <unp2093@naver.com>
2021-11-26 12:12:04 +0900

    'step1'

---
+++ 11_alb.tf
@@ -0,0 +1,10 @@
+resource "aws_lb" "useogi_alb" {
+    name = "useogi-alb"
+    internal = false
+    load_balancer_type = "application"
+    security_groups = [aws_security_group.useogi_sg.id]
+    subnets = [aws_subnet.useogi_puba.id,aws_subnet.useogi_pubc.id]
+    tags = {
+        "Name" = "useogi-alb"
+    }
+}
